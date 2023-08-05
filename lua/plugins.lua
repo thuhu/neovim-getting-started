b6468ae79782e6bd085847eb0f1f4eaa87f8097b
Desmond Letshedi <DesmondL@parity.co.za>
2023-08-05 10:00:24 +0200

    Added telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,20 +14,37 @@ return require("packer").startup(function(use)
 		-- Common Utilities
 		use("nvim-lua/plenary.nvim")
 
-    -- Telescope
-    use({
-        "nvim-telescope/telescope.nvim",
-         tag = "0.1.1",
-         requires = { { "nvim-lua/plenary.nvim" } },
-    })
+		-- Telescope
+		use({
+			"nvim-telescope/telescope.nvim",
+			 tag = "0.1.1",
+			 requires = { { "nvim-lua/plenary.nvim" } },
+		})
 
 		use({ 'rose-pine/neovim', as = 'rose-pine' })
 
 		use({ 
-				'nvim-treesitter/nvim-treesitter', 
-				config = function() 
-					require('configs.treesitter')
-				end,
-				{ run = ':TSUpdate'}
+			'nvim-treesitter/nvim-treesitter', 
+			config = function() 
+				require('configs.treesitter')
+			end,
+			{ run = ':TSUpdate'}
 		}) 
+
+		-- LSP
+		use ({
+		  'VonHeikemen/lsp-zero.nvim',
+		  branch = 'v2.x',
+		  requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
+		  }
+		})
 end)
