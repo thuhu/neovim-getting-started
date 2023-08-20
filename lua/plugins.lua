93410bc1aed920982f80160b65538e89eff2d387
Desmond Letshedi <Desmond@outlook.com>
2023-08-20 21:00:41 +0200

    Added lualine

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,41 +1,41 @@
 -- Automatically run: PackerCompile
-	
+
 vim.api.nvim_create_autocmd("BufWritePost", {
-				group = vim.api.nvim_create_augroup("PACKET", { clear = true }),
-				pattern = "plugin.lua",
-				command = "source <afile> | PackerCompile"
+	group = vim.api.nvim_create_augroup("PACKET", { clear = true }),
+	pattern = "plugin.lua",
+	command = "source <afile> | PackerCompile"
 })
 
 return require("packer").startup(function(use)
 
-		-- Packer
-		use("wbthomason/packer.nvim")
+	-- Packer
+	use("wbthomason/packer.nvim")
 
-		-- Common Utilities
-		use("nvim-lua/plenary.nvim")
+	-- Common Utilities
+	use("nvim-lua/plenary.nvim")
 
-		-- Telescope
-		use({
-			"nvim-telescope/telescope.nvim",
-			 tag = "0.1.1",
-			 requires = { { "nvim-lua/plenary.nvim" } },
-		})
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-		use({ 'rose-pine/neovim', as = 'rose-pine' })
+	use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-		use({ 
-			'nvim-treesitter/nvim-treesitter', 
-			config = function() 
-				require('configs.treesitter')
-			end,
-			{ run = ':TSUpdate'}
-		}) 
+	use({
+		'nvim-treesitter/nvim-treesitter',
+		config = function()
+			require('configs.treesitter')
+		end,
+		{ run = ':TSUpdate'}
+	})
 
-		-- LSP
-		use ({
-		  'VonHeikemen/lsp-zero.nvim',
-		  branch = 'v2.x',
-		  requires = {
+	-- LSP
+	use ({
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v2.x',
+		requires = {
 			-- LSP Support
 			{'neovim/nvim-lspconfig'},             -- Required
 			{'williamboman/mason.nvim'},           -- Optional
@@ -45,7 +45,12 @@ return require("packer").startup(function(use)
 			{'hrsh7th/nvim-cmp'},     -- Required
 			{'hrsh7th/cmp-nvim-lsp'}, -- Required
 			{'L3MON4D3/LuaSnip'},     -- Required
-		  }
-		})
-		use("theprimeagen/harpoon")
+		}
+	})
+	use("theprimeagen/harpoon")
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true
+	}
+}
 end)
