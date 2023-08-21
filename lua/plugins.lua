b296549f4827395e827674b7d3f20a1e688ea67a
Desmond Letshedi <DesmondL@parity.co.za>
2023-08-21 08:06:48 +0200

    Use kanagawa theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -20,7 +20,8 @@ return require("packer").startup(function(use)
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
-	use({ 'rose-pine/neovim', as = 'rose-pine' })
+	-- use({ 'rose-pine/neovim', as = 'rose-pine' })
+	use "rebelot/kanagawa.nvim"
 
 	use({
 		'nvim-treesitter/nvim-treesitter',
